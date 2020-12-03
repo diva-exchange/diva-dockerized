c4fdb87d25549a41c56ad464aeaea06d07d024b7
Konrad Baechler <konrad@diva.exchange>
2020-12-03 18:43:08 +0100

    testnet with 7 nodes, new scripts

---
+++ bin/start-testnet.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2020 diva.exchange
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Author/Maintainer: Konrad Bächler <konrad@diva.exchange>
+#
+# -e  Exit immediately if a simple command exits with a non-zero status
+set -e
+
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
+cd ${PROJECT_PATH}
+PROJECT_PATH=`pwd`/
+
+NODES=${NODES:-7}
+
+DOMAIN=${DOMAIN:-testnet.diva.i2p}
+NAME_NETWORK=${NAME_NETWORK:-network.${DOMAIN}}
+
+NAME_EXPLORER=${NAME_EXPLORER:-explorer.${DOMAIN}}
+IP_EXPLORER=172.20.101.3
+
+NAME_I2P=${NAME_I2P:-i2p.${DOMAIN}}
+IP_I2P=172.20.101.4
+
+NAME_NODE=${NAME_NODE:-node.${DOMAIN}}
+IP_NODE=172.20.101.5
+
+# network
+echo "Creating network ${NAME_NETWORK}..."
+if [[ ! `docker network ls | fgrep ${NAME_NETWORK}` ]]
+then
+  docker network create \
+    --driver bridge \
+    --ipam-driver default \
+    --subnet 172.20.101.0/24 \
+    ${NAME_NETWORK} \
+    >/dev/null
+fi
+
+# i2p
+echo "Starting ${NAME_I2P}..."
+docker run \
+  --detach \
+  --name ${NAME_I2P} \
+  --restart unless-stopped \
+  --network ${NAME_NETWORK} \
+  --ip ${IP_I2P} \
+  --env ENABLE_TUNNELS=1 \
+  --volume ${NAME_I2P}:/home/i2pd/data/ \
+  --volume ${PROJECT_PATH}tunnels.conf.d/testnet:/home/i2pd/tunnels.source.conf.d/ \
+  divax/i2p:latest \
+  >/dev/null
+
+# postgres and iroha
+IP_IROHA_START=120
+ADD_HOSTS=""
+NO_PROXY=""
+for (( t=1; t<=${NODES}; t++ ))
+do
+  IP_IROHA=172.20.101.$(( ${IP_IROHA_START} + ${t} ))
+  ADD_HOSTS="${ADD_HOSTS}--add-host n${t}.${DOMAIN}:${IP_IROHA}"
+  [[ ${t} -lt ${NODES} ]] && ADD_HOSTS="${ADD_HOSTS} "
+  NO_PROXY="${NO_PROXY}n${t}.${DOMAIN}"
+  [[ ${t} -lt ${NODES} ]] && NO_PROXY="${NO_PROXY},"
+done
+echo "ADD_HOSTS: ${ADD_HOSTS}"
+echo "NO_PROXY: ${NO_PROXY}"
+
+IP_POSTGRES_START=20
+for (( t=1; t<=${NODES}; t++ ))
+do
+  echo "Starting n${t}.db.${DOMAIN}..."
+  IP_POSTGRES=172.20.101.$(( ${IP_POSTGRES_START} + ${t} ))
+  docker run \
+    --detach \
+    --name n${t}.db.${DOMAIN} \
+    --restart unless-stopped \
+    --stop-timeout 5 \
+    --network ${NAME_NETWORK} \
+    --ip ${IP_POSTGRES} \
+    --env POSTGRES_DATABASE=iroha \
+    --env POSTGRES_USER=iroha \
+    --env POSTGRES_PASSWORD=iroha \
+    --volume n${t}.db.${DOMAIN}:/var/lib/postgresql/data/ \
+    postgres:10-alpine \
+    >/dev/null
+
+  echo "Starting n${t}.${DOMAIN}..."
+  IP_IROHA=172.20.101.$(( ${IP_IROHA_START} + ${t} ))
+  docker run \
+    ${ADD_HOSTS} \
+    --detach \
+    --name n${t}.${DOMAIN} \
+    --restart unless-stopped \
+    --stop-timeout 5 \
+    --network ${NAME_NETWORK} \
+    --ip ${IP_IROHA} \
+    --env IP_POSTGRES=${IP_POSTGRES} \
+    --env NAME_DATABASE=iroha \
+    --env NAME_PEER=n${t} \
+    --env BLOCKCHAIN_NETWORK=local \
+    --env NO_PROXY=${NO_PROXY} \
+    --volume n${t}.${DOMAIN}:/opt/iroha/ \
+    divax/iroha:latest \
+    >/dev/null
+done
+
+echo "Starting ${NAME_NODE}..."
+docker run \
+  --detach \
+  --name ${NAME_NODE} \
+  --restart unless-stopped \
+  --network ${NAME_NETWORK} \
+  --ip ${IP_NODE} \
+  --env NODE_ENV=development \
+  --env TORII=172.20.101.121:50051 \
+  --env CREATOR_ACCOUNT=diva@testnet.diva.i2p \
+  --env I2P_HOSTNAME=${IP_I2P} \
+  --env I2P_HTTP_PROXY_PORT=4444 \
+  --env I2P_WEBCONSOLE_PORT=7070 \
+  --volume ${NAME_NODE}:/home/node/data/ \
+  --volume n1.${DOMAIN}:/tmp/iroha/ \
+  divax/iroha-node:api-ws \
+  >/dev/null
+
+# explorer
+echo "Starting ${NAME_EXPLORER}..."
+docker run \
+  --detach \
+  --name ${NAME_EXPLORER} \
+  --restart unless-stopped \
+  --network ${NAME_NETWORK} \
+  --ip ${IP_EXPLORER} \
+  --env IP_EXPLORER=0.0.0.0 \
+  --env PORT_EXPLORER=3920 \
+  --env PATH_IROHA=/tmp/iroha/ \
+  --volume n1.${DOMAIN}:/tmp/iroha/:ro \
+  divax/iroha-explorer:latest \
+  >/dev/null
---
+++ bin/stop-testnet.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2020 diva.exchange
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Author/Maintainer: Konrad Bächler <konrad@diva.exchange>
+#
+# -e  Exit immediately if a simple command exits with a non-zero status
+set -e
+
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
+cd ${PROJECT_PATH}
+PROJECT_PATH=`pwd`/
+
+NODES=${NODES:-7}
+DOMAIN=${DOMAIN:-testnet.diva.i2p}
+NAME_NETWORK=${NAME_NETWORK:-network.${DOMAIN}}
+
+NAME_EXPLORER=${NAME_EXPLORER:-explorer.${DOMAIN}}
+NAME_I2P=${NAME_I2P:-i2p.${DOMAIN}}
+NAME_NODE=${NAME_NODE:-node.${DOMAIN}}
+
+# explorer
+echo "Stopping ${NAME_EXPLORER}..."
+[[ `docker ps | fgrep ${NAME_EXPLORER}` ]] && docker stop ${NAME_EXPLORER} >/dev/null
+[[ `docker ps -a | fgrep ${NAME_EXPLORER}` ]] && docker rm ${NAME_EXPLORER} >/dev/null
+
+# node
+echo "Stopping ${NAME_NODE}..."
+[[ `docker ps | fgrep ${NAME_NODE}` ]] && docker stop ${NAME_NODE} >/dev/null
+[[ `docker ps -a | fgrep ${NAME_NODE}` ]] && docker rm ${NAME_NODE} >/dev/null
+[[ `docker volume ls | fgrep ${NAME_NODE}` ]] && docker volume rm ${NAME_NODE} >/dev/null
+
+# postgres and iroha container
+for (( t=1; t<=${NODES}; t++ ))
+do
+  echo "Stopping n${t}.${DOMAIN}..."
+  [[ `docker ps | fgrep n${t}.${DOMAIN}` ]] && docker stop n${t}.${DOMAIN} >/dev/null
+  echo "Stopping n${t}.db.${DOMAIN}..."
+  [[ `docker ps | fgrep n${t}.db.${DOMAIN}` ]] && docker stop n${t}.db.${DOMAIN} >/dev/null
+  [[ `docker ps -a | fgrep n${t}.${DOMAIN}` ]] && docker rm n${t}.${DOMAIN} >/dev/null
+  [[ `docker ps -a | fgrep n${t}.db.${DOMAIN}` ]] && docker rm n${t}.db.${DOMAIN} >/dev/null
+  [[ `docker volume ls | fgrep n${t}.db.${DOMAIN}` ]] && docker volume rm n${t}.db.${DOMAIN} >/dev/null
+done
+
+# i2p
+echo "Stopping ${NAME_I2P}..."
+[[ `docker ps | fgrep ${NAME_I2P}` ]] && docker stop ${NAME_I2P} >/dev/null
+[[ `docker ps -a | fgrep ${NAME_I2P}` ]] && docker rm ${NAME_I2P} >/dev/null
+
+#network
+echo "Removing network ${NAME_NETWORK}..."
+[[ `docker network ls | fgrep ${NAME_NETWORK}` ]] && docker network rm ${NAME_NETWORK} >/dev/null
