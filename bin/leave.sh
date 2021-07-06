9068275ffeefb35b400e5bc4fca3684b7663320c
Konrad Baechler <konrad@diva.exchange>
2021-07-06 15:16:52 +0200

    add: leave network script

---
+++ bin/leave.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2021 diva.exchange
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Author/Maintainer: Konrad Bächler <konrad@diva.exchange>
+#
+# -e  Exit immediately if a simple command exits with a non-zero status
+set -e
+
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
+cd ${PROJECT_PATH}
+PROJECT_PATH=`pwd`/
+
+# env vars
+BASE_DOMAIN=${BASE_DOMAIN:-testnet.diva.i2p}
+
+# load helpers
+source "${PROJECT_PATH}bin/echos.sh"
+source "${PROJECT_PATH}bin/helpers.sh"
+
+if ! command_exists docker-compose; then
+  error "docker-compose not available. Please install it first.";
+  exit 3
+fi
+
+info "Stopping..."
+sudo docker-compose -f build/${BASE_DOMAIN}.yml down -d
